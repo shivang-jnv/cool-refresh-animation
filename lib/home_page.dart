53c103479aecca3b06c281cc54cd1636de6c05a7
shivang-jnv <skjnvspn@gmail.com>
2023-09-30 13:06:31 +0530

    cool pull to refresh animation

---
+++ lib/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+
+class HomePage extends StatelessWidget {
+  Future<void> _handleRefresh() async {
+    //reloading takes some time
+    return await Future.delayed(
+      Duration(seconds: 2),
+    );
+  }
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: LiquidPullToRefresh(
+          height: 200,
+          color: Colors.deepPurple,
+          onRefresh: _handleRefresh,
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
